edeb20bce4708786dc7266c73366019c5183de57
gondosik <semenisin@gmail.com>
2021-11-05 23:15:19 +0100

    drawing circle ready

--- UkolGrafikaSlu/UkolGrafikaSlu.cpp
+++ UkolGrafikaSlu/UkolGrafikaSlu.cpp
@@ -14,7 +14,8 @@ const int WINDOW_HEIGHT = 500;
 
 double getStep(double, double);
 double getD(double, double);
-void drawLine(double, double, double, double , SDL_Renderer*);
+void drawLineDDA(double, double, double, double , SDL_Renderer*);
+void drawCircleUsingLines(double, SDL_Renderer*);
 
 double getStep(double dx, double dy) {
 	return ((dx > dy) ? dx : dy);
@@ -22,7 +23,7 @@ double getStep(double dx, double dy) {
 double getD(double num0, double num1) {
 	return num1 - num0;
 }
-void drawLine(double x0, double y0, double x1, double y1, SDL_Renderer* renderer) {
+void drawLineDDA(double x0, double y0, double x1, double y1, SDL_Renderer* renderer) {
 	double x, y, i, dx, dy, XP, YP, steps;
 	int MX, MY;
 	
@@ -53,14 +54,6 @@ void drawLine(double x0, double y0, double x1, double y1, SDL_Renderer* renderer
 	x = x0;
 	y = y0;
 
-	std::cout << "Data o promennych: " << std::endl;
-	std::cout << "Pocatek: [" << x << ";" << y << "]" << std::endl;
-	std::cout << "Pocet kroku: " << steps << std::endl;
-	std::cout << "Prirustek X: " << XP << std::endl;
-    std::cout << "Prirustek Y: " << YP << std::endl;
-	std::cout << "Hodnota MX: " << MX << std::endl;
-	std::cout << "Hodnota MY: " << MY << std::endl;
-
 	XP = (XP * MX);
 	YP = (YP * MY);
 
@@ -76,9 +69,30 @@ void drawLine(double x0, double y0, double x1, double y1, SDL_Renderer* renderer
 		SDL_RenderPresent(renderer);
 	}
 }
+
+void drawCircleUsingLines(double r, SDL_Renderer *renderer) {
+	double circleXcenter = WINDOW_WIDTH / 2;
+	double circleYcenter = WINDOW_HEIGHT / 2;
+	double alpha = 1 / r;
+	double sinAlpha = sin(alpha);
+	double cosAlpha = cos(alpha);
+	double x0 = (r * cosAlpha);  
+	double y0 = (r * sinAlpha);
+	double x1 = 0.0, y1 = 0.0;
+
+	for (double angle = alpha; angle < 90.0; angle += alpha) {
+		x1 = (x0 * cosAlpha) - (y0 * sinAlpha);
+		y1 = (x0 * sinAlpha) + (y0 * cosAlpha);
+	    drawLineDDA(x0 + circleXcenter, y0 + circleYcenter, x1 + circleXcenter, y1 + circleYcenter, renderer);
+		SDL_RenderPresent(renderer);
+		x0 = x1;
+		y0 = y1;
+	}
+
+}
 int main(int argc, char** args) {
 	HWND consoleWindow = GetConsoleWindow();
-	double x0 = 0.0, y0 = 0.0, x1 = 0.0, y1 = 0.0;
+	double r = 0.0;
 	SDL_Surface* winSurface = NULL;
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
@@ -103,25 +117,13 @@ int main(int argc, char** args) {
 	std::cout << "Hodnoty museji mit hodnotu vetsi nez 0!" << std::endl << "Maji datovy typ double." << std::endl;
 
 	do {
-		std::cout << "Zadejte souradnici x0 prvniho bodu" << std::endl;
-		std::cin >> x0;
-	} while (!x0);
-	do {
-		std::cout << "Zadejte souradnici y0 prvniho bodu" << std::endl;
-		std::cin >> y0;
-	} while (!y0);
-	do {
-		std::cout << "Zadejte souradnici x1 druheho bodu" << std::endl;
-		std::cin >> x1;
-	} while (!x1);
-	do {
-		std::cout << "Zadejte souradnici y1 druheho bodu" << std::endl;
-		std::cin >> y1;
-	} while (!y1);
+		std::cout << "Zadejte hodnotu polomeru r kruznice" << std::endl;
+		std::cin >> r;
+	} while (!r);
 
-	std::cout << "Vykresluji primku na zaklade bodu A[" << x0 << ";" << y0 << "] a B[" << x1 << ";" << y1 << "]." << std::endl;
+	std::cout << "Vykresluji kruznici na zaklade polomeru r " << r << " a prirustku alfa " << (1/r)  << " °." << std::endl;
 	std::cout << std::endl;
-	drawLine(x0,y0,x1,y1, renderer);
+	drawCircleUsingLines(r, renderer);
 
 	if (!window) {
 		std::cout << "Chyba pri vytvareni grafickeho okna: " << SDL_GetError() << std::endl;
